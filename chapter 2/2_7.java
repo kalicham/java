73d71e49f3cd8a74528d7bec2e862c4f296fa7fb
kalicham <32410283+kalicham@users.noreply.github.com>
2019-02-11 13:05:43 +0600

    Add files via upload

---
+++ chapter 2/2_7.java
@@ -0,0 +1,17 @@
+/*
+(Convert feet into meters) Write a program that reads a number in feet, converts it
+to meters, and displays the result. One foot is 0.305 meter. Here is a sample run:
+*/
+import java.util.Scanner;
+
+public class Lesson2_7 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number of minutes: ");
+		int minutes = input.nextInt();
+		int days = minutes % 525600 % 1440;
+		int years = minutes / 525600;
+		System.out.println(minutes + " minutes is approximately " + years
+		 + " years and " + days + " days");
+	}
+}
